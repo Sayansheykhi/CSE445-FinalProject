13331bf864c478540d05077bb73187a25870a42c
Sayansheykhi <ssheykhi@asu.edu>
2025-11-24 15:05:32 -0800

    firstCommit

---
+++ Assignment5/Sajjad-Sheykhi-a5/UtilityWebApp/Default.aspx.designer.cs
@@ -0,0 +1,15 @@
+namespace UtilityWebApp
+{
+    public partial class Default
+    {
+        protected global::System.Web.UI.WebControls.Label lblVisits;
+        protected global::System.Web.UI.WebControls.TextBox txtHashInput;
+        protected global::System.Web.UI.WebControls.Button btnHash;
+        protected global::System.Web.UI.WebControls.Label lblHashOutput;
+        protected global::System.Web.UI.WebControls.TextBox txtZipcode;
+        protected global::System.Web.UI.WebControls.Button btnGetTemp;
+        protected global::System.Web.UI.WebControls.Label lblTempOutput;
+        protected global::System.Web.UI.WebControls.GridView gvServiceDirectory;
+    }
+}
+
